e7619d6431a2c133be724aef69fd4f3ce183d75f
orionscript10 <orionscript10@gmail.com>
2026-05-11 12:54:09 +0700

    Fix: Tambah error handling pada proses delete menu

--- proses/proses_delete_menu.php
+++ proses/proses_delete_menu.php
@@ -4,13 +4,21 @@ $id = (isset($_POST['id'])) ? htmlentities($_POST['id']) : '';
 $foto = (isset($_POST['foto'])) ? htmlentities($_POST['foto']) : '';
 
 if (!empty($_POST['input_menu_validate']) && !empty($id)) {
-    unlink("../assets/img/$foto");
+    // Hapus file foto jika ada
+    $file_path = "../assets/img/$foto";
+    if (file_exists($file_path)) {
+        unlink($file_path);
+    }
+    
+    // Delete dari database
     $query = mysqli_query($conn, "DELETE FROM tb_daftar_menu WHERE id = '$id'");
     if ($query) {
         $message = '<script>alert("Data menu berhasil dihapus!"); window.location = "../menu";</script>';
     } else {
         $message = '<script>alert("Data menu gagal dihapus!"); window.location = "../menu";</script>';
     }
+} else {
+    $message = '<script>alert("Data tidak valid!"); window.location = "../menu";</script>';
 }
 echo $message;
 ?>
